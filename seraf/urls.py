448dc53061e5ae1fb20853584383d9cc2e43a307
Eirik Krogstad <eirikkr@gmail.com>
2014-02-25 18:53:05 +0100

    Added plumbing urls

--- seraf/urls.py
+++ seraf/urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
     url(r'^', include('content.urls')),
 
     url(r'^api/vault/', include('vault.urls')),
+    url(r'^api/plumbing/', include('plumbing.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
