87ac307d948bd4ef4e6112271dc7433fa1155bc8
Eirik Krogstad <eirikkr@gmail.com>
2014-03-16 11:46:20 +0100

    Added mistune markdown filter

---
+++ content/templatetags/markdown.py
@@ -0,0 +1,10 @@
+from django import template
+from django.utils.safestring import mark_safe
+import mistune
+
+register = template.Library()
+
+@register.filter
+def markdown(value):
+    html = mistune.markdown(value)
+    return mark_safe(html)
