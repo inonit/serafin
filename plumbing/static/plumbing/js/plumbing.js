b7b15841950fbfa02b57bb34a01b0a510ff2856a
Eirik Krogstad <eirikkr@gmail.com>
2014-04-18 12:58:30 +0200

    Fixed load/save and graph model management for plumbing

--- plumbing/static/plumbing/js/plumbing.js
+++ plumbing/static/plumbing/js/plumbing.js
@@ -72,32 +72,10 @@ plumbing.run(['$rootScope', '$timeout', 'jsPlumb', function(scope, timeout, jsPl
     scope.currentNoderef = '';
 
     jsPlumb.ready(function() {
-        scope.jsPlumb.bind('dblclick', function(c) {
-            scope.jsPlumb.detach(c);
-        });
 
-        scope.jsPlumb.bind('connection', function(c) {
-            c.connection.getOverlays()[1].label = c.connection.getParameters()['label'];
-            scope.data.edges.push({
-                label: 'next',
-                source: +c.sourceId.substr(5),
-                target: +c.targetId.substr(5),
-            });
-        });
-
-        scope.jsPlumb.bind('connectionDetached', function(c) {
-            var index;
-            scope.data.edges.forEach(function(edge) {
-                if (edge.source == +c.sourceId.substr(5) &&
-                    edge.target == +c.targetId.substr(5)) {
-                    index = scope.data.edges.indexOf(edge);
-                }
-            });
-            scope.data.edges.splice(index, 1);
-        });
-
-        scope.jsPlumb.doWhileSuspended(function() {
-            if (scope.data.edges) {
+        // timeout to avoid race condition
+        timeout(function() {
+            scope.jsPlumb.doWhileSuspended(function() {
                 scope.data.edges.forEach(function(edge) {
                     scope.jsPlumb.connect({
                         source: 'node_' + edge['source'],
@@ -107,19 +85,50 @@ plumbing.run(['$rootScope', '$timeout', 'jsPlumb', function(scope, timeout, jsPl
                         }
                     });
                 });
-            }
+            });
+
+            scope.jsPlumb.bind('dblclick', function(c) {
+                scope.jsPlumb.detach(c);
+            });
+
+            // set overlay and push to scope on connection
+            scope.jsPlumb.bind('connection', function(c) {
+                c.connection.getOverlays()[1].label = c.connection.getParameters()['label'];
+                scope.$apply(function() {
+                    scope.data.edges.push({
+                        label: 'next',
+                        source: +c.sourceId.substr(5),
+                        target: +c.targetId.substr(5),
+                    });
+                });
+            });
+
+            // remove edge on detach
+            scope.jsPlumb.bind('connectionDetached', function(c) {
+                scope.data.edges.forEach(function(edge) {
+                    if (edge.source == +c.sourceId.substr(5) &&
+                        edge.target == +c.targetId.substr(5)) {
+                        scope.$apply(function() {
+                            scope.data.edges.splice(
+                                scope.data.edges.indexOf(edge), 1
+                            );
+                        });
+                    }
+                });
+            });
+
         });
     });
 }]);
 
-plumbing.controller('graph', ['$scope', '$http', function(scope, http) {
+plumbing.controller('graph', ['$scope', 'jsPlumb', function(scope, jsPlumbService) {
 
     scope.addNode = function() {
         var id = scope.data.nodes.length + 1;
 
-        scope.currentNoderef = 'noderef_' + id;
         scope.data.nodes.push({
             'id': id,
+            'ref_id': '',
             'url': newNodeUrl,
             'title': '?',
             'metrics': {
@@ -128,6 +137,8 @@ plumbing.controller('graph', ['$scope', '$http', function(scope, http) {
             }
         });
 
+        // immediately opens a django popup to select the source for the node
+        scope.currentNoderef = 'noderef_' + id;
         window.open(
             addNodeUrl + '?t=id&_popup=1',
             scope.currentNoderef,
@@ -136,6 +147,8 @@ plumbing.controller('graph', ['$scope', '$http', function(scope, http) {
     };
 
     scope.deleteNode = function(index) {
+        var node = scope.data.nodes[index];
+        scope.jsPlumb.detachAllConnections('node_' + node.id);
         scope.data.nodes.splice(index, 1);
     };
 
@@ -147,8 +160,10 @@ plumbing.directive('node', ['jsPlumb', function(jsPlumbService) {
         controller: 'graph',
         scope: '@',
         link: function(scope, element, attrs) {
+            // set id here to avoid race condition
             element[0].id = 'node_' + scope.node.id;
 
+            // set up jsPlumb for this node
             jsPlumb.ready(function() {
                 scope.jsPlumb.draggable(element);
                 scope.jsPlumb.doWhileSuspended(function() {
@@ -157,6 +172,7 @@ plumbing.directive('node', ['jsPlumb', function(jsPlumbService) {
                 });
             });
 
+            // apply changes in metrics when a drag move ends
             element.bind('mouseup', function(e) {
                 scope.$apply(function() {
                     scope.node.metrics.left = element[0].style.left;
@@ -164,10 +180,11 @@ plumbing.directive('node', ['jsPlumb', function(jsPlumbService) {
                 });
             });
 
+            // opens a django popup on double click
             element.bind('dblclick', function() {
                 window.open(
                     scope.node.url + '?_popup=1',
-                    'noderef-' + scope.node.id,
+                    'noderef_' + scope.node.id,
                     'height=800,width=1024,resizable=yes,scrollbars=yes'
                 ).focus();
             });
@@ -179,13 +196,17 @@ plumbing.directive('noderef', ['$http', function(http) {
     return {
         restrict: 'C',
         link: function(scope, element, attrs) {
+            // set id here to avoid race condition
             element[0].id = 'noderef_' + scope.node.id;
 
+            // focus will return to the window when a popup closes
+            // if the value of a noderef has changed, it will not be picked up automatically
+            // add it to the scope if it has changed
             angular.element(window).bind('focus', function(val) {
                 var value = element.attr('value');
-                if (value !== scope.node.id) {
+                if (value !== scope.node.ref_id) {
                     scope.$apply(function() {
-                        scope.node.id = value;
+                        scope.node.ref_id = value;
                     });
                     http.get(pageApiUrl + value).success(function(data) {
                         scope.node.title = data['title'];
