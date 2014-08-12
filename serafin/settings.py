290670c33f41a258ba23b274491098b7e818a12e
Eirik Krogstad <eirikkr@gmail.com>
2014-08-12 14:19:10 +0200

    Remove excessive logging

--- serafin/settings.py
+++ serafin/settings.py
@@ -303,10 +303,6 @@ LOGGING = {
         },
     },
     'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
         'huey_log': {
             'level': 'DEBUG',
             'class': 'logging.handlers.RotatingFileHandler',
@@ -315,22 +311,11 @@ LOGGING = {
             'backupCount': 2,
             'formatter': 'standard',
         },
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': [],
-            'class': 'django.utils.log.AdminEmailHandler',
-            'include_html': True,
-        }
     },
     'loggers': {
         'huey.consumer': {
             'handlers': ['huey_log'],
             'level': 'DEBUG',
-            'propagate': True,
-        },
-         'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
             'propagate': True,
         },
     }
