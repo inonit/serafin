ccc75497c736d8511a90578bf0c39d83149e325d
Rolf Håvard Blindheim <rhblind@gmail.com>
2015-03-26 13:13:14 +0100

    added endpoint for searching variables

--- system/urls.py
+++ system/urls.py
@@ -3,12 +3,12 @@ from __future__ import unicode_literals
 from django.conf.urls import patterns, url, include
 from rest_framework import routers
 
-from system.views import VariableViewSet
+from system.views import VariableViewSet, VariableSearchViewSet
 
 
 router = routers.DefaultRouter()
+router.register(r'variables/search', VariableSearchViewSet)
 router.register(r'variables', VariableViewSet)
-
 
 urlpatterns = patterns(
     '',
--- system/views.py
+++ system/views.py
@@ -1,12 +1,25 @@
-from __future__ import unicode_literals
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, unicode_literals
 
 from rest_framework import viewsets
 
-from system.models import Variable
-from system.serializers import VariableSerializer
+from .models import Variable
+from .serializers import VariableSerializer
+from .filters import VariableSearchFilter
 
 
 class VariableViewSet(viewsets.ModelViewSet):
 
     queryset = Variable.objects.all()
     serializer_class = VariableSerializer
+
+
+class VariableSearchViewSet(viewsets.ModelViewSet):
+
+    queryset = Variable.objects.all()
+    serializer_class = VariableSerializer
+    filter_backends = [VariableSearchFilter]
+    search_fields = ["name", "display_name"]
+
+
