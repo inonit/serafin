133fa96b7551f7f5dc3a1f2e0e6885f57f0c51fe
Eirik Krogstad <eirikkr@gmail.com>
2015-04-10 12:53:58 +0200

    Fix ordering and slashes of urls

--- content/urls.py
+++ content/urls.py
@@ -4,6 +4,6 @@ from events import receivers
 urlpatterns = patterns('',
     url(r'^$', 'content.views.get_session', name='content'),
     url(r'^api/$', 'content.views.get_page', name='content_api'),
-    url(r'^api/(?P<content_type>\w+)/(?P<file_id>\d+)$', 'content.views.api_filer_file', name='api_filer_file'),
-    url(r'^(?P<route_slug>\w+)$', 'content.views.content_route', name='content_route'),
+    url(r'^api/(?P<content_type>\w+)/(?P<file_id>\d+)/$', 'content.views.api_filer_file', name='api_filer_file'),
+    url(r'^(?P<route_slug>\w+)/$', 'content.views.content_route', name='content_route'),
 )
