da19c96f3f803a42c5010dcddfe979b085338228
Rolf Håvard Blindheim <rhblind@gmail.com>
2015-04-13 21:53:10 +0200

    #108 - work in progress

--- staticfiles/js/string-expression.js
+++ staticfiles/js/string-expression.js
@@ -6,7 +6,7 @@
 "use strict";
 
 
-angular.module("stringExpression", [])
+angular.module("stringExpression", ["autocompleteSearch"])
 
     .config(function($httpProvider) {
         $httpProvider.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
@@ -16,12 +16,54 @@ angular.module("stringExpression", [])
         return {
             restrict: "E",
             replace: true,
-            transclude: true,
+            transclude: false,
             require: "?ngModel",
-            scope: {},
+            scope: {
+                url: "@url"
+            },
             templateUrl: "template/string-expression.html",
-            controller: ["$scope", function($scope) {
+            controller: ["$scope", "$timeout", "ExpressionValidatorService", "QueryService", "QueueService",
+                function($scope, $timeout, ExpressionValidatorService, QueryService, QueueService) {
 
-            }]
+                QueueService.configure({timeout: 200});
+
+                function fetch(url, queryString) {
+                    QueryService.fetch(url, queryString).then(function(response) {
+                        $scope.expression.response = response;
+                    })
+                }
+
+                $scope.addQuery = function(url) {
+                   if (ExpressionValidatorService.isValid($scope.expression.query)) {
+                       QueueService.add(_.partial(fetch, url, $scope.expression.query));
+                   } else if (!$scope.expression.query.length) {
+                       $scope.expression.response = {};
+                   }
+                };
+
+                $scope.expression = {
+                    query: "",
+                    response: {}
+                };
+            }],
+            link: function(scope, element, attrs) {
+                var input = element.children("input[type=text]");
+            }
         }
+    })
+
+    .service("ExpressionValidatorService", function() {
+
+        function isValid(expressionString) {
+            /* Implement some kind of client side validation
+             * before posting to the server.
+             * */
+
+            return true;
+        }
+
+        return {
+            isValid: isValid
+        }
+
     });
