ad8645cb72218ce3ba79758d14a5c0a4b6f7ef2d
Eirik Krogstad <eirikkr@gmail.com>
2015-06-04 12:40:32 +0200

    Remove Expressions, leaving Parser, update and simplify tests

--- system/expressions.py
+++ system/expressions.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Serafin Expression mini-language implementation details.
+Serafin Parser mini-language implementation details.
 
 Variables:
   Variables must be prefixed with `$` like eg. $MyLittlePony.
@@ -9,19 +9,18 @@ Variables:
   will be raised.
 
 Operators:
-  We have two sets of expression types; MathExpressions and BoolExpressions.
-  Expression evaluation between mixed types are not supported, and each Expression
+  We have two sets of expression types; math expressions and boolean expressions.
+  Expression evaluation between mixed types are not supported, and each expression
   type supports a predefined set of operators.
 
-  BoolExpression operators:
+  Boolean expression operators:
     - == (eq)   - >= (ge)
     - != (ne)   - in (in/contains)
     - <  (lt)   - &  (AND)
     - <= (le)   - |  (OR)
     - >  (gt)   - !  (NOT)
 
-
-  MathExpression operators:
+  Math expression operators:
     - +  (add)              - %  (mod/modulo)
     - /  (truediv)          - *  (multiply)
     - ^  (power/exponent)   - -  (subtract)
@@ -44,12 +43,11 @@ Grouping:
 
 In order to resolve user variables, a User instance must be passed
 to the Parser on initialization. If the Parser is initialized without
-a User instance, attempts to lookup variables will raise an error.
+a User instance, attempts to lookup undefined variables will raise an error.
 """
 
 from __future__ import absolute_import, unicode_literals
 
-import functools
 import math
 import operator as oper
 from sys import float_info
@@ -67,175 +65,14 @@ from pyparsing import (
 )
 
 
-def chain(func):
-    """
-    Function wrapper that clones and returns a new
-    instance of the class in order to make functions chainable.
-    """
-
-    @functools.wraps(func)
-    def wrapper(self, *args, **kwargs):
-        self = self._clone(lhs=self.eval, operator=func(self, *args, **kwargs))
-        return self
-
-    return wrapper
-
-
-@functools.total_ordering
-class _Expression(object):
-    """
-    Base class for simple expressions.
-    The class supports method chaining for defined operators.
-    """
-
-    operators = None
-
-    def __init__(self, lhs, operator, rhs):
-
-        self.lhs = lhs  # Left Hand Side
-        self.rhs = rhs  # Right Hand Side
-
-        try:
-            self.operator = self._get_operator(operator.lower())
-        except KeyError:
-            raise AttributeError("Invalid operator parameter '%s'. Valid operators are %s." %
-                                 (operator, ", ".join(self.operators.keys())))
-
-    def __call__(self, param):
-        """
-        Sets the right hand parameter based on the output from
-        the chained operator call.
-        """
-        self.rhs = getattr(param, "eval", param)
-        return self
-
-    @chain
-    def __getattr__(self, name):
-        """
-        Returns the chained operator function
-        """
-        if name in self.operators:
-            return self.operators[name]
-        raise AttributeError(
-            "'%(cls)s' object has no attribute '%(name)s'" % {
-                "cls": self.__class__.__name__,
-                "name": name
-            })
-
-    def __repr__(self):
-        return "<%(cls)s: %(lhs)s '%(operator)s' %(rhs)s>" % {
-            "cls": self.__class__.__name__, "lhs": self.lhs,
-            "operator": self.operator.__name__, "rhs": self.rhs
-        }
-
-    def __eq__(self, other):
-        return self.eval == other
-
-    def __lt__(self, other):
-        return self.eval < other
-
-    def __nonzero__(self):
-        return self.eval
-
-    __bool__ = __nonzero__
-
-    def _clone(self, **kwargs):
-        """
-        Returns a new clone of self with updated values
-        from kwargs.
-        """
-        klass = self.__class__.__new__(self.__class__)
-        klass.__dict__ = self.__dict__.copy()
-        klass.__dict__.update(**kwargs)
-        return klass
-
-    def _get_operator(self, operator):
-        if not self.operators:
-            raise AttributeError(
-                "%(cls)s has no attribute 'operators'. Define a %(cls)s.operators "
-                "dictionary or override the `_get_operator()` function." % {
-                    "cls": self.__class__.__name__
-                }
-            )
-        return self.operators[operator]
-
-    @property
-    def eval(self):
-        return self.operator(self.lhs, self.rhs)
-
-
-class BoolExpression(_Expression):
-    """
-    Simple class for abstracting boolean expressions.
-    Example:
-        BoolExpression(
-            lhs="NOBODY expects the Spanish Inquisition!",
-            operator="eq",
-            rhs="NOBODY expects the Spanish Inquisition!"
-        ).eval
-
-    Expressions can be chained ie. like this:
-        BoolExpression(4, "gt", 3).and_(BoolExpression(["apple", "fish"], "in", "fish")).eval
-        would evaluate to (4 > 3) and ("fish" in ["apple", "fish"])
-
-    Valid operators are
-      eq, ne, lt, le, gt, ge, in, contains (alias in), and_, or_.
-    """
-    operators = {
-        "eq": oper.eq,
-        "ne": oper.ne,
-        "lt": oper.lt,
-        "le": oper.le,
-        "gt": oper.gt,
-        "ge": oper.ge,
-        "in": oper.contains,
-        "contains": oper.contains,
-        "and_": oper.and_,
-        "or_": oper.or_,
-        "not_": oper.not_
-    }
-
-
-class MathExpression(_Expression):
-    """
-    Simple class for abstracting simple mathematical expressions.
-    Example:
-        MathExpression(
-            lhs=1,
-            operator="add",
-            rhs=2
-        ).eval
-
-    Expressions can be chained ie. like this:
-        MathExpression(10, "add", 5).sub(1).div(MathExpression(1, "add", 1)).eval
-        would evaluate to ((10 + 5) - 1) / (1 + 1)
-
-    Valid operators are
-      add, div, floordiv, pow, exponent (alias pow), mod,
-      modulo (alias mod), multiply, sub, subtract (alias sub).
-    """
-
-    operators = {
-        "add": oper.add,
-        "div": oper.truediv,
-        "floordiv": oper.floordiv,
-        "pow": oper.pow,
-        "exponent": oper.pow,
-        "mod": oper.mod,
-        "modulo": oper.mod,
-        "multiply": oper.mul,
-        "sub": oper.sub,
-        "subtract": oper.sub
-    }
-
-
 class Parser(object):
     """
     A parser for parsing and evaluating expressions using our
     little expression mini-language.
 
-    This code is pretty much stolen from
+    This code is partially taken from
     https://pyparsing.wikispaces.com/file/view/fourFn.py
+    and other pyparsing examples
     """
 
     UNARY = "unary -"
--- system/tests/test_expressions.py
+++ system/tests/test_expressions.py
@@ -6,186 +6,22 @@ import math
 
 from django.test import TestCase
 from django.utils import timezone
-from rest_framework.test import APIRequestFactory
 
 from users.models import User
 
-from ..expressions import Parser, BoolExpression
-
-factory = APIRequestFactory()
-
-
-class BoolExpressionTestCase(TestCase):
-
-    # "eq" operator
-    def test_boolexpression_string_eq(self):
-        self.assertTrue(BoolExpression(
-            lhs="NOBODY expects the Spanish Inquisition!",
-            operator="eq",
-            rhs="NOBODY expects the Spanish Inquisition!"
-        ))
-        self.assertFalse(BoolExpression("Foo", "eq", "Bar"))
-
-    def test_boolexpression_int_eq(self):
-        self.assertTrue(BoolExpression(1, "eq", 1))
-        self.assertFalse(BoolExpression(1, "eq", 2))
-
-    def test_boolexpression_bool_eq(self):
-        self.assertTrue(BoolExpression(True, "eq", True))
-        self.assertFalse(BoolExpression(True, "eq", False))
-
-    # "ne" operator
-    def test_boolexpression_string_ne(self):
-        self.assertTrue(BoolExpression("Foo", "ne", "Bar"))
-        self.assertFalse(BoolExpression("Foo", "ne", "Foo"))
-
-    def test_boolexpression_int_ne(self):
-        self.assertTrue(BoolExpression(1, "ne", 2))
-        self.assertFalse(BoolExpression(1, "ne", 1))
-
-    def test_boolexpression_bool_ne(self):
-        self.assertTrue(BoolExpression(True, "ne", False))
-        self.assertFalse(BoolExpression(True, "ne", True))
-
-    # "lt" operator
-    def test_boolexpression_string_lt(self):
-        # Strings are compared by ASCII values
-        self.assertTrue(
-            ord("a") < ord("b")
-        )
-        self.assertTrue(BoolExpression("a", "lt", "b"))
-        self.assertFalse(BoolExpression("b", "lt", "a"))
-
-    def test_boolexpression_int_lt(self):
-        self.assertTrue(BoolExpression(1, "lt", 2))
-        self.assertFalse(BoolExpression(1, "lt", 1))
-        self.assertFalse(BoolExpression(2, "lt", 1))
-
-    def test_boolexpression_bool_lt(self):
-        self.assertTrue(BoolExpression(False, "lt", True))
-        self.assertFalse(BoolExpression(True, "lt", False))
-
-    # "le" operator
-    def test_boolexpression_string_le(self):
-        self.assertTrue(
-            ord("a") <= ord("b")
-        )
-        self.assertTrue(BoolExpression("a", "le", "a"))
-        self.assertTrue(BoolExpression("a", "le", "b"))
-        self.assertFalse(BoolExpression("b", "le", "a"))
-
-    def test_boolexpression_int_le(self):
-        self.assertTrue(BoolExpression(1, "le", 1))
-        self.assertTrue(BoolExpression(1, "le", 2))
-        self.assertFalse(BoolExpression(2, "le", 1))
-
-    def test_boolexpression_bool_le(self):
-        self.assertTrue(BoolExpression(False, "le", True))
-        self.assertTrue(BoolExpression(False, "le", False))
-        self.assertTrue(BoolExpression(True, "le", True))
-        self.assertFalse(BoolExpression(True, "le", False))
-
-    # "gt" operator
-    def test_boolexpression_string_gt(self):
-        self.assertTrue(
-            ord("b") > ord("a")
-        )
-        self.assertTrue(BoolExpression("b", "gt", "a"))
-        self.assertFalse(BoolExpression("a", "gt", "b"))
-
-    def test_boolexpression_int_gt(self):
-        self.assertTrue(BoolExpression(2, "gt", 1))
-        self.assertFalse(BoolExpression(1, "gt", 2))
-
-    def test_boolexpression_bool_gt(self):
-        self.assertTrue(BoolExpression(True, "gt", False))
-        self.assertFalse(BoolExpression(False, "gt", True))
-
-    # "ge" operator
-    def test_boolexpression_string_ge(self):
-        self.assertTrue(
-            ord("b") >= ord("a")
-        )
-        self.assertTrue(BoolExpression("a", "ge", "a"))
-        self.assertTrue(BoolExpression("b", "ge", "a"))
-        self.assertFalse(BoolExpression("a", "ge", "b"))
-
-    def test_boolexpression_int_ge(self):
-        self.assertTrue(BoolExpression(1, "ge", 1))
-        self.assertTrue(BoolExpression(2, "ge", 1))
-        self.assertFalse(BoolExpression(1, "ge", 2))
-
-    def test_boolexpression_bool_ge(self):
-        self.assertTrue(BoolExpression(True, "ge", False))
-        self.assertTrue(BoolExpression(False, "ge", False))
-        self.assertTrue(BoolExpression(True, "ge", False))
-        self.assertFalse(BoolExpression(False, "ge", True))
-
-    # "in" operator
-    def test_boolexpression_string_in(self):
-        self.assertTrue(BoolExpression("The Castle of aaarrrrggh", "in", "aaarrrrggh"))
-        self.assertFalse(BoolExpression("The Castle of aaarrrrggh", "in", "AAARRRRGGH"))
-
-    def test_boolexpression_list_in(self):
-        l = ["Blasphemy!", "He", "said", "it", "again"]
-        self.assertTrue(BoolExpression(l, "in", "Blasphemy!"))
-        self.assertFalse(BoolExpression(l, "in", "Parrot"))
-
-    def test_boolexpression_dict_in(self):
-        d = {"a herring": "Then you must cut down the mightiest tree in the forest with..."}
-        self.assertTrue(BoolExpression(d, "in", "a herring"))
-        self.assertFalse(BoolExpression(d, "in", "an axe"))
-
-    def test_boolexpression_bool_in(self):
-        self.assertTrue(BoolExpression([True, False], "in", True))
-        self.assertFalse(BoolExpression([False], "in", True))
-
-    # "and" operator
-    def test_boolexpression_string_and(self):
-        self.assertTrue(BoolExpression("fish" in ["apple", "fish"], "and_", True))
-        self.assertFalse(BoolExpression("fish" in ["apple", "fish"], "and_", False))
-
-    def test_boolexpression_int_and(self):
-        self.assertTrue(BoolExpression(1 < 2, "and_", 10 < 20))
-        self.assertFalse(BoolExpression(1 > 2, "and_", 10 < 20))
-
-    def test_boolexpression_bool_and(self):
-        self.assertTrue(BoolExpression(True, "and_", True))
-        self.assertFalse(BoolExpression(False, "and_", True))
-
-    # "or" operator
-    def test_boolexpression_string_or(self):
-        self.assertTrue(BoolExpression("fish" in ["apple"], "or_", "apple" in ["apple", "cake"]))
-        self.assertFalse(BoolExpression("parrot" in ["I", "like", "rat", "cake"], "or_", False))
-
-    # To be continued...
-
-    def test_boolexpression_simple_chain(self):
-        self.assertTrue(BoolExpression(True, "eq", True).and_(True))
-        self.assertTrue(BoolExpression(True, "or_", False).or_(True))
-
-    def test_boolexpression_complex_chain(self):
-        self.assertTrue(BoolExpression(4, "gt", 3).and_(BoolExpression(["apple", "fish"], "in", "fish")))
-
-
-class MathExpressionTestCase(TestCase):
-
-    # TODO: Write me
-    pass
+from ..expressions import Parser
 
 
 class ParserTestCase(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create(**{
-            "username": 1,
-            "data": {
-                "UserVar1": 1,
-                "UserVar2": 2,
-                "UserVar3": "I like traffic lights",
-                "UserVar4": ""
-            }
-        })
+        self.user = User()
+        self.user.data = {
+            "UserVar1": 1,
+            "UserVar2": 2,
+            "UserVar3": "I like traffic lights",
+            "UserVar4": ""
+        }
         self.parser = Parser(user_obj=self.user)
         self.now = timezone.localtime(timezone.now().replace(microsecond=0))
 
