1c55126815a0a882bce8583989979d4ae38fb498
Eirik Krogstad <eirikkr@gmail.com>
2015-06-11 12:27:55 +0200

    Slightly unnecessary defaults migration

---
+++ system/migrations/0012_auto_20150610_1723.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import jsonfield.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('system', '0011_content_program'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='content',
+            name='data',
+            field=jsonfield.fields.JSONField(default=[]),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='content',
+            name='program',
+            field=models.ForeignKey(blank=True, to='system.Program', help_text='Can optionally be bound to a specific program', null=True, verbose_name='program'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='session',
+            name='data',
+            field=jsonfield.fields.JSONField(default='{"nodes": [], "edges": []}'),
+            preserve_default=True,
+        ),
+    ]
