fd44a35ab73fab85dc240503f538c8a2d9bb0079
Bjørn Tore Håvie <bjorn.haavie@inonit.no>
2017-04-09 19:17:12 +0200

    2m timeout, run worker in the same container for now

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,12 +10,16 @@ fi
 
 python manage.py collectstatic --noinput # TODO: Move to docker build?
 
+echo Starting worker.
+python manage.py run_huey &
+
 # Start Gunicorn processes
 echo Starting Gunicorn.
 exec gunicorn serafin.wsgi:application \
     --name serafin_django \
     --bind 0.0.0.0:8000 \
     --workers 5 \
+    --timeout 120 \
     --log-level=info \
     --log-file=- \
     --access-logfile=- \
