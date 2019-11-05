e7164982c5e66ad50c05ab0b96b21041ee686ee2
Gal <weizenberg@gmail.com>
2019-11-05 12:17:13 +0200

    remove ptvsd from manage.py run it using supervisord for deubugging purpose

--- manage.py
+++ manage.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python
 import os
 import sys
-import ptvsd
-import time
-
-
-if "0.0.0.0:8000" in sys.argv:
-    print "a"
-    ptvsd.enable_attach()
-    print "b"
-    #ptvsd.wait_for_attach()
-    #time.sleep(5)
-    print "c"
-    #add --noreload flag to enable debuging with ptvsd
 
 
 if __name__ == "__main__":
