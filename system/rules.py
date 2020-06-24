8dcbbb48d03ad3c7022578c2fee0cd50f7858ca3
Gal <weizenberg@gmail.com>
2020-06-24 14:28:47 +0300

    fix view permission link

--- system/rules.py
+++ system/rules.py
@@ -20,26 +20,37 @@ def has_program_related_access(user, related):
 
 rules.add_perm('system', rules.always_allow)
 rules.add_perm('system.change_program', has_program_access)
+rules.add_perm('system.view_program', has_program_access)
 
 rules.add_perm('system.add_variable', has_program_related_access)
 rules.add_perm('system.change_variable', has_program_related_access)
 rules.add_perm('system.delete_variable', has_program_related_access)
+rules.add_perm('system.view_variable', has_program_related_access)
 
 rules.add_perm('system.add_programuseraccess', has_program_related_access)
 rules.add_perm('system.change_programuseraccess', has_program_related_access)
 rules.add_perm('system.delete_programuseraccess', has_program_related_access)
+rules.add_perm('system.view_programuseraccess', has_program_related_access)
 
 rules.add_perm('system.add_session', has_program_related_access)
 rules.add_perm('system.change_session', has_program_related_access)
 rules.add_perm('system.delete_session', has_program_related_access)
+rules.add_perm('system.view_session', has_program_related_access)
 
 rules.add_perm('system.add_content', has_program_related_access)
 rules.add_perm('system.change_content', has_program_related_access)
 rules.add_perm('system.delete_content', has_program_related_access)
+rules.add_perm('system.view_content', has_program_related_access)
 
 rules.add_perm('users.change_user', has_program_related_access)
 rules.add_perm('users.delete_user', has_program_related_access)
 
+rules.add_perm('system.add_chapter', has_program_related_access)
 rules.add_perm('system.change_chapter', has_program_related_access)
+rules.add_perm('system.delete_chapter', has_program_related_access)
+rules.add_perm('system.view_chapter', has_program_related_access)
 
+rules.add_perm('system.add_module', has_program_related_access)
 rules.add_perm('system.change_module', has_program_related_access)
+rules.add_perm('system.delete_module', has_program_related_access)
+rules.add_perm('system.view_module', has_program_related_access)
